70a4aa304b9053f6244fde2070c3f63401ca1647
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2024-09-21 01:48:36 +0530

    server creation and uploadthing done also prisma set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["uploadthing.com", "utfs.io"], // Add utfs.io here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/a/bspl1n3yy6/*",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
